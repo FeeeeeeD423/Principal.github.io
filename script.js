4f28fd5cd2a1c84370204d4091c3e9c49e63821c
FeeeeeeD423 <150744506+FeeeeeeD423@users.noreply.github.com>
2023-11-16 15:44:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function toggleSubMenu(menuId) {
+    // Oculta todos los sub-menus primero
+    document.querySelectorAll('.sub-buttons-container').forEach(function(subMenu) {
+        subMenu.style.display = 'none';
+    });
+
+    // Muestra el sub-menu clickeado si estaba previamente oculto
     var subMenu = document.getElementById(menuId);
     if (subMenu.style.display === "none" || subMenu.style.display === "") {
-        subMenu.style.display = "block";
-    } else {
-        subMenu.style.display = "none";
+        subMenu.style.display = "flex";
     }
 }
