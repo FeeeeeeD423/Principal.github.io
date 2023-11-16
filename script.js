640a27b16f1121de6edd9ed6bf22aa125321b847
FeeeeeeD423 <150744506+FeeeeeeD423@users.noreply.github.com>
2023-11-16 15:18:32 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function showCategory(category) {
-    // Aquí va la lógica para mostrar el contenido de cada categoría
-    // Por ejemplo, puedes cambiar el contenido del div 'category-content'
-    document.getElementById('category-content').innerHTML = 'Contenido de ' + category;
-}
